63d9214ab6fad7c3a2441b31e21b2b140b713476
abiezzisamir2020 <42677955+abiezzisamir2020@users.noreply.github.com>
2019-03-21 21:37:58 -0700

    Add files via upload

---
+++ BankCreation/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Steps to a Bank!");
+        //create accounts (checking, savings, investing, etc.)
+        //create tax rates
+        //create credit rates
+        //provide security
+        //take info of member
+        //make personal info private and public info public
+    }
+}
